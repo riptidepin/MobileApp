8131c50ebe6b42528e2d3ec0128cfb387c44a153
riptidepin <unjhaylee122301@gmail.com>
2023-05-16 09:03:35 +0800

    react native first machine problem

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
-      <Text>Barbie</Text>
+      <Text>Angeli Mhiriel Severo</Text>
       <StatusBar style="auto" />
     </View>
   );
